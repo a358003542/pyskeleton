95b0892392784742e489970967ea8a542b734d6c
万泽 <a358003542@gmail.com>
2015-04-21 12:49:44 +0800

    2015-04-21 12:49

--- setup.py
+++ setup.py
@@ -16,33 +16,33 @@ os.replace('skeleton.tar.gz','skeleton/skeleton.tar.gz')
 
 
 setup(
-  name='skeleton',
-  version='0.1',
-  description='the software short description',
-  url='https://github.com/a358003542/skeleton',
-  author='wanze',
-  author_email='a358003542@gmail.com',
-  maintainer = 'wanze',
-  maintainer_email = 'a358003542@gmail.com',
-  license='GPL 2',
-  platforms = 'Linux',
-  keywords =['skeleton','python'],
-  classifiers = ['Development Status :: 2 - Pre-Alpha',
-  'Environment :: Console',
-  'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-  'Operating System :: POSIX :: Linux',
-  'Programming Language :: Python :: 3.4',],
-  packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-  include_package_data=True,
-  package_data = {"skeleton":['skeleton.tar.gz'],},
-#  install_requires=['click'],
-#  setup_requires,
-#  extras_require={'test': ['pytest'],},
-  entry_points = {
-  'console_scripts' :[ 'skeleton=skeleton.main:console',],
-#  'gui_scripts':['xskeleton=skeleton.main:gui'],
-  }
-)
+    name='skeleton',
+    version='0.1',
+    description='the software short description',
+    url='https://github.com/a358003542/skeleton',
+    author='wanze',
+    author_email='a358003542@gmail.com',
+    maintainer = 'wanze',
+    maintainer_email = 'a358003542@gmail.com',
+    license='GPL 2',
+    platforms = 'Linux',
+    keywords =['skeleton','python'],
+    classifiers = ['Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.4',],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    package_data = {"skeleton":['skeleton.tar.gz'],},
+#   install_requires=['click'],
+#   setup_requires,
+#   extras_require={'test': ['pytest'],},
+    entry_points = {
+        'console_scripts' :[ 'skeleton=skeleton.main:console',],
+#       'gui_scripts':['xskeleton=skeleton.main:gui'],
+        }
+    )
 
 
 
