d52ac7f2eb81c5606b339e50747de2c51485f2d4
万泽 <a358003542@gmail.com>
2015-05-26 09:40:34 +0800

    删去不必要的文件夹

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os.path
 #anditional to make  a tar.gz file
 import tarfile
 with tarfile.open("skeleton.tar.gz", "w:gz") as tar:
-    for name in ["setup.py","LICENSE","README.md","skeleton", "tests"]:
+    for name in ["setup.py","LICENSE","README.md","skeleton"]:
         tar.add(name)
 import os
 os.replace('skeleton.tar.gz','skeleton/skeleton.tar.gz')
@@ -17,7 +17,7 @@ os.replace('skeleton.tar.gz','skeleton/skeleton.tar.gz')
 
 setup(
     name='skeleton',
-    version='0.1',
+    version='0.11',
     description='the software short description',
     url='https://github.com/a358003542/skeleton',
     author='wanze',
